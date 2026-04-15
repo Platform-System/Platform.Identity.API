c32a049c636a3832d441b75dbb3f994e377795c7
Khánh Hưng <khanhhungzm@gmail.com>
2026-04-15 16:48:34 +0700

    refactor: use shared api authentication module

--- Program.cs
+++ Program.cs
@@ -1,62 +1,17 @@
-using Keycloak.AuthServices.Authentication;
-using Microsoft.OpenApi.Models;
+using Platform.Api.Extensions;
 using Platform.Identity.API.Infrastructure.Extensions;
-using System.IdentityModel.Tokens.Jwt;
-
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityServices(builder.Configuration, typeof(Program).Assembly);
-
-builder.Services.AddKeycloakAuthentication(builder.Configuration);
-
-builder.Services.AddAuthorization();
-
 builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(option =>
-{
-    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Platform Identity API", Version = "v1" });
-
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter token format: {token}"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string [] { }
-        }
-    });
-});
+builder.Services.AddPlatformAuthentication(builder.Configuration);
+builder.Services.AddPlatformSwaggerJwt("Platform Identity API");
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthentication();
-app.UseAuthorization();
+app.UsePlatformSwagger();
+app.UsePlatformAuthentication();
 
 app.MapControllers();
 
